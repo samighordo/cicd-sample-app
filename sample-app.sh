9e9eaf39b9de3b924b198463e05b64d0eac09bd8
samighordo <79763131+samighordo@users.noreply.github.com>
2023-10-16 21:00:01 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -24,9 +24,13 @@ _EOF_
 
 cd tempdir || exit
 
+docker stop samplerunning
 docker rm samplerunning
+docker stop samplerunning2
 docker rm samplerunning2
+docker stop samplerunning3
 docker rm samplerunning3
+docker stop samplerunning4
 docker rm samplerunning4
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning5 sampleapp
