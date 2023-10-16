38cf43a58e6435aeedf5e0d233ebbc51fdcb94d1
samighordo <79763131+samighordo@users.noreply.github.com>
2023-10-16 21:25:54 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -29,5 +29,5 @@ docker stop samplerunning
 docker rm samplerunning
 
 docker build -t sampleapp .
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker run -t -d -p 5050:5050 --name samplerunning2 sampleapp
 docker ps -a 
